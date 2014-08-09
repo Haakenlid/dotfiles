4391adfa9124b100b79be13c2f593198dc99ed7c
Håken Lid <haakenlid@gmail.com>
2014-08-09 22:44:50 +0200

    virtualenvwrapper removed. Needs virtualenv to be installed to work properly.

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -7,11 +7,12 @@ export LESS=-XR
 complete -W "$(echo $(cat ~/.bash_history | sed -n 's/^ssh \([^ ]*@[^ ]*\).*/\1/p'|sort -u))" ssh sshfs
 
 # sets up virtualenvwrapper
-export WORKON_HOME=~/.venvs/
-export VIRTUALENVWRAPPER_LOG_DIR="$WORKON_HOME"
-export VIRTUALENVWRAPPER_HOOK_DIR="$WORKON_HOME"
-source ~/.dotfiles/bin/virtualenvwrapper.sh
-
+if [[ -e /usr/local/bin/virtualenvwrapper.sh ]]; then
+  export WORKON_HOME=~/.venvs/
+  export VIRTUALENVWRAPPER_LOG_DIR="$WORKON_HOME"
+  export VIRTUALENVWRAPPER_HOOK_DIR="$WORKON_HOME"
+  source /usr/local/bin/virtualenvwrapper.sh
+fi
 
 export PYTHONPATH=$PYTHONPATH:/usr/bin/
 

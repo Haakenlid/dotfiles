997235c20bae958a98ec7212073fb38c26c6ed5d
Håken Lid <haakenlid@gmail.com>
2014-08-10 17:57:55 +0200

    mekket i timesvis på historyscript ...

--- source/40_history.sh
+++ source/40_history.sh
@@ -34,71 +34,85 @@ export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
 alias timestamp="date -d 'today' +'%Y-%m-%d_%H:%M:%S'"
 
-function clean_history() {
-    mkdir -p $HOME/.backup
-    BACKUP=$HOME/.backup/.bash_history_$(timestamp).backup
-    cp $HOME/.bash_history $BACKUP
-    NEW_HISTORY=$(cat $HOME/.bash_history)
+function cleanhistory() {
+    local HISTORY="$HOME/.bash_history"
+    local BACKUP="$HOME/.backup/.bash_history_$(timestamp).backup"
+    local NEW_HISTORY=$(cat $HISTORY)
     for REMOVETERM in $@; do
-            NEW_HISTORY=$(echo "$NEW_HISTORY" | grep -vi "$REMOVETERM")
+        NEW_HISTORY=$(echo "$NEW_HISTORY" | grep -vi "$REMOVETERM")
     done
-    NEW_HISTORY=$(echo "$NEW_HISTORY" | sed 's/ $//' |nl|sort -k2 -k1|uniq -f1|sort -n|cut -f2|grep "^.\{10,200\}$"|tac|uniq -w2|tac)
-    echo "$NEW_HISTORY" > $HOME/.bash_history
+    NEW_HISTORY=$(echo "$NEW_HISTORY"|sed 's/ *$//'|tac|nl -n rz|sort -k2 -k1|uniq -f1|sort -n|cut -f2|grep "^.\{10,200\}$"|uniq -w2|tac )
+    mkdir -p $HOME/.backup
+    cp "$HISTORY" "$BACKUP"
+    echo "$NEW_HISTORY" > "$HISTORY"
+
+    echo -e "$(wc -l $BACKUP $HISTORY | sed -e '$d')"
     echo -e "History logs have been streamlined."
-    echo -e "$(wc -l $BACKUP $HOME/.bash_history | sed -e '$d')"
 }
 
-function undo_clean_history() {
-    NEWEST_BACKUP=$(ls $HOME/.backup/.bash_history_* -1t|head -n 1)
+function undo_cleanhistory() {
+    local HISTORY="$HOME/.bash_history"
+    local NEWEST_BACKUP=$(ls $HOME/.backup/.bash_history_* -1t|head -n 1)
+
+    echo -e "$(wc -l $NEWEST_BACKUP $HISTORY | sed -e '$d')"
+    mv "$NEWEST_BACKUP" "$HISTORY"
     echo -e "History cleanup rolled back."
-    echo -e "$(wc -l $NEWEST_BACKUP $HOME/.bash_history | sed -e '$d')"
-    mv $NEWEST_BACKUP $HOME/.bash_history
 }
 
 function search_for_terms_in_history {
-    HISTTIMEFORMAT="" # we don't want timestamps. Matches are chronological anyway.
-    MATCH_COLORS=('01;49;36' '01;49;33' '01;49;32' '01;49;35' '01;49;31' '01;49;34')
-    LINENUMBERCOLOR='01;49;39'
-    LINENUMBERPATTERN='^.{7}' # change this if column count changes for line numbers
-    number_of_colors=${#MATCH_COLORS[@]}
+    local HISTORY_FILE="$HOME/.bash_history"
+    local TEMP_HISTORY_FILE="/tmp/.bash_history"
+    local MATCH_COLORS=( '01;49;33' '01;49;31' '01;49;32' '01;49;35' '01;49;34' '01;49;36' )
+    local LINENUMBERCOLOR='01;49;39'
+    local COLOR_INDEX=0
+    local SEARCHTERMS=''
+    local HITS_LENGTH=0
+    local DELIMITER='#CACHE'
     # lists command history, purging duplicates and stripping space at end of lines.
-    MATCHES=$(history | grep -v '  ??' | sed 's/ $//' | sort -k2 | tac | uniq -f1 | sort -n )
-    color_index=0
-    SEARCHTERMS=''
+    local RESULT=$(HISTTIMEFORMAT='' history | sed 's/^ *[0-9]*//'| nl -n rz | sed 's/ \+$//' | sort -k2 | tac | uniq -f2 | sort | tac )
+    # prepares history for searching.
+    local HITS=$(echo "$RESULT" | sed 's/\s*[0-9]*\s\+//' | grep -v '^??' )
     for SEARCHTERM in $@; do
-        (( color_index = (color_index + 1) % ${#MATCH_COLORS[@]} ))
-        COLOR=${MATCH_COLORS[color_index]}
-        # fixes it so that you can use ^ regexp pattern.
-        SEARCHTERMS+=' \e['$COLOR'm'$SEARCHTERM
-        SEARCHTERM=$(echo $SEARCHTERM | sed "s/^\^/\(?<=$LINENUMBERPATTERN\)/" )
+        # uses different color for each search term.
+        local COLOR=${MATCH_COLORS[COLOR_INDEX]}
+        (( COLOR_INDEX = (COLOR_INDEX + 1) % ${#MATCH_COLORS[@]} ))
         # searches and highlights searchterm with color
-        MATCHES=$(echo "$MATCHES" | GREP_COLOR=$COLOR grep -Pi --color=always "$SEARCHTERM")
+        HITS=$(echo "$HITS" | GREP_COLOR=$COLOR grep -Pi --color=always "$SEARCHTERM")
+        # uses same color for summary.
+        SEARCHTERMS+=' \e['$COLOR'm'$SEARCHTERM
     done
 
     if [ -z $1 ]; then
         # no searchterm - lets just display last ten entries.
-        MATCHES=$(echo "$MATCHES" | tail -n 10 )
+        RESULT=$(history | tail -n 10 )
+    else
+        RESULT=$( echo "$HITS" | nl --number-width=3 --number-separator='  ' | tac )
+        # prepend to .bash_history so you can type !1 instead of !3412 to replay.
+        echo "$HITS" | sed -r 's/\x1b\[[0-9;]*[Km]//g' > "$TEMP_HISTORY_FILE"
+        echo "$DELIMITER" >> "$TEMP_HISTORY_FILE"
+        if [[ $(grep "$DELIMITER" "$HISTORY_FILE") ]]; then
+            cat "$HISTORY_FILE" | sed "0,/^$DELIMITER$/d" >> "$TEMP_HISTORY_FILE"
+        else
+            cat "$HISTORY_FILE" >> "$TEMP_HISTORY_FILE"
+        fi
+        mv -f "$TEMP_HISTORY_FILE" "$HISTORY_FILE"
+        # shows same numbers in output
     fi
-
     # highlight line numbers
-    MATCHES=$(echo "$MATCHES" | GREP_COLOR=$LINENUMBERCOLOR grep -P --color=always "$LINENUMBERPATTERN")
-
+    RESULT=$(echo "$RESULT" | sed 's/\([1-9]\)/!\1/' | GREP_COLOR=$LINENUMBERCOLOR grep -P --color=always '^ *[0-9!]+')
     if [ -z $1 ]; then
         echo -e "Search in bash command history."
         echo -e "Usage: ?? [terms to look for]"
         echo -e "With no arguments lists 10 newest commands from history."
         echo
-        echo -e "$MATCHES"
+        echo -e "$RESULT"
     else
-        if [[ $MATCHES != "" ]]; then
-            TREFF=$(echo "$MATCHES" | wc -l )
+        if [[ $RESULT != "" ]]; then
+            HITS_LENGTH=$(echo "$RESULT" | wc -l )
             echo -e "\e["$LINENUMBERCOLOR"m"
-            echo -e "$MATCHES"
-        else
-            # zero matches
-            TREFF=0
+            echo -e "$RESULT"
         fi
-        echo -e "\nSearch for$SEARCHTERMS\e[0m returned $TREFF results"
+        echo -e "\nSearch for$SEARCHTERMS\e[0m returned $HITS_LENGTH hits."
     fi
 }
 

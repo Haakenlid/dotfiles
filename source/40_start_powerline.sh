f88d7e8e45ca789fc494bedb2eadb9cedfa3f9aa
Håken Lid <haakenlid@gmail.com>
2014-08-10 18:50:24 +0200

    tweak

--- source/40_start_powerline.sh
+++ source/40_start_powerline.sh
@@ -1,6 +1,6 @@
 # Enables powerline support for tmux and bash
 # must be started before prompt.
 export POWERLINE_NO_BASH_PROMPT="True"
-export PATH="$PATH$HOME/.dotfiles/powerline/scripts:"
 source ~/.dotfiles/powerline/powerline/bindings/bash/powerline.sh
+export PATH="$PATH:$HOME/.dotfiles/powerline/scripts:"
 

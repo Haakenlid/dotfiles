d9b5e6a9ecc898b332f8d1c8c93380aea9b9caad
Håken Lid <haakenlid@gmail.com>
2014-11-21 21:23:25 +0100

    nocolor alias

--- source/50_aliases.sh
+++ source/50_aliases.sh
@@ -2,5 +2,6 @@ alias v='sptfy volume'
 alias n='sptfy next'
 alias b='sptfy prev'
 alias p='sptfy play'
+alias nocolor='sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"'
 
 # alias cd!='cd $(fc -r | grep -m1 -o "/.*/")'

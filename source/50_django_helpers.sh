7fd307a2ada68c9d1700582d76070911e660e339
Håken Lid <haakenlid@gmail.com>
2014-12-11 08:22:12 +0100

    Snarveier til django-management selv om man ikke har aktivert venv.

--- source/50_django_helpers.sh
+++ source/50_django_helpers.sh
@@ -1,18 +1,33 @@
 ######################################################
 # Django helper functions
 
+_workon()
+{
+  command -v django-admin >/dev/null 2>&1 || fc -s workon
+}
+
+notebook()
+{
+  _workon
+  fc -s 'django-admin shell_plus --notebook'
+  reset
+}
+
 runserver()
 {
-    django-admin runserver_plus
-    reset
+  _workon
+  django-admin runserver_plus
+  reset
 }
 
 shell()
 {
-    django-admin shell_plus
+  _workon
+  django-admin shell_plus
 }
 
 migrate()
 {
-    django-admin makemigrations && django-admin migrate
+  _workon
+  django-admin makemigrations && django-admin migrate
 }

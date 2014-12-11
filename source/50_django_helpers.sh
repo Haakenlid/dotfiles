515af5889fb792a8471acf0e931c5be702fa2619
Håken Lid <haakenlid@gmail.com>
2014-12-11 07:33:43 +0100

    Endret hjelpefunksjoner / aliaser for django

--- source/50_django_helpers.sh
+++ source/50_django_helpers.sh
@@ -4,10 +4,15 @@
 runserver()
 {
     django-admin runserver_plus
+    reset
 }
-
 
 shell()
 {
     django-admin shell_plus
+}
+
+migrate()
+{
+    django-admin makemigrations && django-admin migrate
 }

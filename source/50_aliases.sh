cfd69713c4e53006616506f61e35f99a47eef63a
Håken Lid <haakenlid@gmail.com>
2015-02-03 17:17:24 +0100

    fixed spotify aliases for cli

--- source/50_aliases.sh
+++ source/50_aliases.sh
@@ -1,7 +1,17 @@
-alias v='sptfy volume'
-alias n='sptfy next'
-alias b='sptfy prev'
-alias p='sptfy play'
+# Spotifiy shortcuts
+
+program_exists () {
+  type "$1" &> /dev/null ;
+}
+
+if program_exists spotify; then
+  alias v='sptfy volume'  # usage v [+, ++, --, 0, 3, 4, max]
+  alias n='sptfy next'
+  alias b='sptfy back'
+  alias p='sptfy play'  # plauy / pause
+fi
+
 alias nocolor='sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"'
 
+# failed experiment
 # alias cd!='cd $(fc -r | grep -m1 -o "/.*/")'

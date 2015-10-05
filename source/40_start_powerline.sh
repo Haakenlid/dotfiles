f1345709b2a14dd1dca0a4efde2faff9b66b92a8
Håken Lid <haakenlid@gmail.com>
2015-10-05 22:22:26 +0200

    lol

--- source/40_start_powerline.sh
+++ source/40_start_powerline.sh
@@ -2,5 +2,5 @@
 # must be started before prompt.
 export POWERLINE_NO_BASH_PROMPT="True"
 export POWERLINE_PATH="$HOME/.dotfiles/libs/powerline"
-source "$POWERLINE_PATH/powerline/bindings/bash/powerline.sh"
 export PATH="$POWERLINE_PATH/scripts:$PATH"
+source "$POWERLINE_PATH/powerline/bindings/bash/powerline.sh"

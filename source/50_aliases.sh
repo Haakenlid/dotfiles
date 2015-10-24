57cf797a8b897fb360a079c2851b891c21cb9d13
Håken Lid <haakenlid@gmail.com>
2015-10-25 01:53:38 +0200

    grep options is deprecated

--- source/50_aliases.sh
+++ source/50_aliases.sh
@@ -1,22 +1,23 @@
-# Spotifiy shortcuts
-
+# Remote servers
 alias UIO='ssh -t haakenl@login.uio.no "cd /uio/kant/div-universitas-desken/; bash"'
-# alias TASSEN='ssh universitas.no'
-# alias FUNKY='ssh funkytime.no'
-alias nor=\
-"setxkbmap -layout no -option caps:escape"\
-" && echo 'norwegian keyboard'"
+alias grep='grep --color=auto'
 
-alias eng=\
-"xkbcomp -I$HOME/.xkb $HOME/.xkb/keymap/en-improved"\
-" $DISPLAY >& /dev/null"\
-" && echo 'english (improved) keyboard'"
+# Keyboard changes
+function nor() {
+  setxkbmap -layout no -option caps:escape
+  echo 'norwegian keyboard'
+}
+function eng() {
+  xkbcomp -I$HOME/.xkb $HOME/.xkb/keymap/en-improved $DISPLAY >& /dev/null
+  echo 'english (improved) keyboard'
+}
 
 program_exists () {
   # test that program exists
   type "$1" &> /dev/null ;
 }
 
+# Spotify shortcuts
 if program_exists spotify; then
   alias sv='sptfy volume'  # usage v [+, ++, --, 0, 3, 4, max]
   alias sn='sptfy next'

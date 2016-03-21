4f62b2a4dc4125ef10b795fd47b593e2301fd3e6
Håken Lid <haakenlid@gmail.com>
2016-03-21 14:58:09 +0100

    not full path for pager = /bin/less, since it doesn't work on RHEL

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -1,8 +1,8 @@
 # Prevent less from clearing the screen while still showing colors.
 export LESS=-XRsiM
-export PAGER=/bin/less
-export MANPAGER=/bin/less
-export CDPATH=.:$HOME:/srv
+export PAGER=less
+export MANPAGER=less
+export CDPATH=.:$HOME
 
 # SSH auto-completion based on entries in history.
 complete -W "$(echo $(cat ~/.bash_history | sed -n 's/^ssh \([^ ]*@[^ ]*\).*/\1/p'|sort -u))" ssh sshfs
@@ -14,10 +14,6 @@ if [[ -e /usr/local/bin/virtualenvwrapper_lazy.sh ]]; then
 fi
 
 export PYTHONPATH="$PYTHONPATH:/usr/bin/"
-
-# access npm packages intalled in home
-# npm config set prefix '~/.npm-packages'
-export PATH="$PATH:$HOME/.npm-packages/bin"
 
 # dont freeze terminal by pressing Ctrl-S
 stty -ixon >& /dev/null

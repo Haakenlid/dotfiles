0834487b61ff5c44f07a901d40e60d1101af4a0e
Håken Lid <haakenlid@gmail.com>
2016-07-27 12:18:00 +0200

    Set EDITOR to nvim if it exists.

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -1,9 +1,12 @@
 # Editing
-export EDITOR="vim"
+if which nvim > /dev/null; then
+  export EDITOR="nvim"
+else
+  export EDITOR="vim"
+fi
 export LESSEDIT="$EDITOR %f"
 export GIT_EDITOR="$EDITOR"
 export VISUAL="$EDITOR"
 # export BROWSER="/usr/bin/w3m"
 
 set -o vi # vi keybindings in bash
-

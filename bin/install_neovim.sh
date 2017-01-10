5f32316ce7030a9b60a3eabf34ff187f1fd6cda8
Håken Lid <haakenlid@gmail.com>
2017-01-10 11:17:50 +0100

    install scripts

--- bin/install_neovim.sh
+++ bin/install_neovim.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt update
-sudo apt install neovim python-pip python3-pip
-sudo pip3 install neovim
-sudo pip2 install neovim
+[ $UID != 0 ] && echo 'must run as root' && exit 1
 
-ln -s ${HOME}/.vim ${HOME}/.config/nvim
-ln -s ${HOME}/.vimrc ${HOME}/.config/nvim/init.vim
+add-apt-repository -y ppa:neovim-ppa/unstable
+apt update
+apt install -y neovim python-pip python3-pip
+pip3 install neovim
+pip2 install neovim
+
+su $(logname) << 'EOF'
+  ln -fs ${HOME}/.vim ${HOME}/.config/nvim
+  ln -fs ${HOME}/.vimrc ${HOME}/.config/nvim/init.vim
+EOF
